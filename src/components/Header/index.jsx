009f0b93a6812d3def9156001677d28a3a4ade67
Gabryel-lima <gabbryellimasi@gmail.com>
2026-03-03 20:49:51 -0300

    Atualizando o caminho das imagens para consitência entre os componentes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ export default function Header({ title, subtitle, children }) {
   return (
 
     <header className="header">
-      <img src= "../../../../public/background.mp4/m2-res_476p.gif" className="header-bg" />
+      <img src= "/background.mp4/m2-res_476p.gif" className="header-bg" />
       <div className="header-overlay"></div>
       <div className="header-content">
         <img src="/senaidexlogo.png" alt="SenaiDex Logo" className="logo" />
